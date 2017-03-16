1abae4dbb3f3e7c08156112382debbfe89af30ba
Ricky Elrod <ricky@elrod.me>
2017-03-16 02:57:24 -0400

    Add constructors for missing opcodes
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- assembler/src/Types.hs
+++ assembler/src/Types.hs
@@ -4,10 +4,15 @@ module Types where
 import Data.Word
 
 data Instruction =
-    NOP
-  | ADDA
-  | LDA
-  | STA
+  ABX | ADCA | ADCB | ADDA | ADDB | ADDD | ANDA | ANDB | ANDCC | ASLA |
+  ASLB | ASL | ASRA | ASRB | ASR | BITA | BITB | CLRA | CLRB | CLR | CMPA |
+  CMPB | CMPD | CMPS | CMPU | CMPX | CMPY | COMA | COMB | COM | CWAI | DAA |
+  DECA | DECB | DEC | EORA | EORB | R1 | R2 | INCA | INCB | INC | JMP | JSR |
+  LDA | LDB | LDD | LDS | LDU | LDX | LDY | LEAS | LEAU | LEAX | LEAY | LSLA |
+  LSLB | LSL | LSRA | LSRB | LSR | MUL | NEGA | NEGB | NEG | NOP | ORA | ORB |
+  ORCC | PSHS | PSHU | PULS | PULU | ROLA | ROLB | ROL | RORA | RORB | ROR |
+  RTI | RTS | SBCA | SBCB | SEX | STA | STB | STD | STS | STU | STX | STY |
+  SUBA | SUBB | SUBD | SWI | SWI2 | SWI3 | SYNC | TFR | TST
   deriving (Eq, Ord, Show)
 
 data Address =
